576e896c8ff5282d9a196cf197b6ebda24b732f0
freelanceTM <isgenderdurdyyew95@gmail.com>
2026-06-06 21:53:26 +0500

    feat(ingredients): Recipe model (product->ingredient pivot with quantity)

---
+++ app/Models/Recipe.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'quantity' => 'float',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function ingredient()
+    {
+        return $this->belongsTo(Ingredient::class);
+    }
+}
